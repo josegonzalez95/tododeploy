365a2661125aa7785ec2743b3a087e295f2e11c3
Jose Gonzalez-Massini <jose.gonzalez95@upr.edu>
2020-12-13 15:12:42 -0400

    added random quotes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import TodoList from './components/TodoList';
+const url = 'https://type.fit/api/quotes';
 
 function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [quote, setQuote] = useState(null);
+  const [isDisplay, setIsDisplay] = useState(false);
+
+  const fetchQuotes = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setQuotes(data);
+  };
+  useEffect(() => {
+    fetchQuotes();
+  }, []);
+
+  const changeQuote = () => {
+    setQuote(quotes[Math.floor(Math.random() * quotes.length)]);
+    setIsDisplay(true);
+  };
+
+  const removeQuote = () => {
+    setIsDisplay(false);
+    setQuote(null);
+  };
   return (
-    <div className='todo-app'>
-      <TodoList></TodoList>
-    </div>
+    <>
+      {quote ? (
+        <div className='quote'>
+          <p>"{quote.text}"</p>
+          <h4>-{quote.author}-</h4>
+        </div>
+      ) : null}
+      <div className='btns'>
+        <button className='btn' onClick={() => changeQuote()}>
+          {isDisplay ? 'Change quote' : 'Display quote'}
+        </button>
+        {isDisplay ? (
+          <button className='btn' onClick={() => removeQuote()}>
+            Remove quote
+          </button>
+        ) : null}
+      </div>
+      <div className='todo-app'>
+        <TodoList></TodoList>
+      </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import background from '/home/jose/Documents/progProjects/newtodo/src/backgroundImg.jpg';
 
 ReactDOM.render(
   <React.StrictMode
     style={{
-      // backgroundImage: `url(${background})`,
       backgroundImage: `url({./backgoundImg})`,
     }}
   >
